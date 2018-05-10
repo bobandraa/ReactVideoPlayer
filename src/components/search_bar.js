5a004c7b9569b5ef31fd4d8aaa6d92997d0b368d
bobandraa <sandra.cohen.ux@gmail.com>
2018-05-10 12:21:00 -0700

    Readded constructor lost in merge

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -19,12 +19,18 @@ import React, { Component } from 'react'; //import 'React' and pull off the prop
 
 // Refactored with arrow functions. Same as above.
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {term: ''};
+  }
+
   render () {
     return (
       <div>
         <input
         value={this.state.term}
-        onChange={evt => console.log(evt.target.value)} />;
+        onChange={evt => this.setState({ term: evt.target.value }) }/>
       </div>
     )
   }

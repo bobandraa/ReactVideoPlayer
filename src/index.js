e70625e0a203e94a7047f7d60eba694cce687045
bobandraa <sandra.cohen.ux@gmail.com>
2018-05-09 17:28:04 -0700

    refactored from functional component to class-based component for search bar' st

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import SearchBar from './components/search_bar'; //Needs relative path when importing anything but libraries
+
 // Create new component that produces HTML.
 
 // const App = function() {
-//   return <div>Hey!!</div>;
-// };
+//   return <div>Hey!!</div>; };
+
 
 // Same as above fuction declaration, but utilizes 'fat arrow' instead of 'function'
 
 const App = () => {
-  return <div>Hey!!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
+
+
+
+
 //Take this component's generated HTML and display on DOM.
 
 ReactDom.render(<App />, document.querySelector('.container'));

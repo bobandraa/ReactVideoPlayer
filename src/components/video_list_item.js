8b0b2a6072cdf1a93e45252a680f6c13fd978906
bobandraa <sandra.cohen.ux@gmail.com>
2018-05-12 18:32:30 -0700

    JSX for video data in video item componement

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import VideoList from './video_list';
+// import VideoList from './video_list';
 
-const VideoListItem = (props) =>{
-  return <li>Video</li>
+const VideoListItem = ({video}) =>{
+  // const video = props.video;  refactored in VideoListItem argument & creates a new variable in component.
+  const imageUrl = video.snippet.thumbnails.default.url;
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl} />
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}
+        </div>
+      </div>
+    </div>
+    </li>
+
+  );
 }
 
 
-export default VideoListItem
+export default VideoListItem;
